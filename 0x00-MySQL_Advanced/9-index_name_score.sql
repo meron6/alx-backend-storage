fb21ee19d9aed41322b8370f81082163600c5382
meron6 <khozameron5@gmail.com>
2024-08-07 11:53:17 -0700

    9-index_name_score.sql

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
--- Task 9: Optimize search and score - this creates an index named idx_name_first_score on the table 'names'
--- that indexes the first letter of the 'name' field and the 'score' field
-CREATE INDEX idx_name_first_score ON names (SUBSTRING(name FROM 1 FOR 1), score);
+-- Task 9: Optimize search and score - creates an index idx_name_first_score on the table names
+-- and the first letter of name and the score
+CREATE INDEX idx_name_first_score ON names ( name(1), score );
