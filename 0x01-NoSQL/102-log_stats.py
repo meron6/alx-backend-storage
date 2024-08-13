abf97d95257b9f96ff1802528078496c9fc30b06
meron6 <khozameron5@gmail.com>
2024-08-13 07:24:30 -0700

    update

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,39 +1,35 @@
 #!/usr/bin/env python3
-"""Log stats
+"""Log stats - new version
 """
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.01:27017')
+    # Connect to MongoDB server
+    client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     path = "path"
     status = "/status"
 
+    # Print the number of logs
     print(f"{nginx_collection.count_documents({})} logs")
     print("Methods:")
 
-    for value in methods:
-        count = nginx_collection.count_documents({"method": value})
-        print(f"\tmethod {value}: {count}")
-    print(f"{nginx_collection.count_documents({path: status})} status check")
-    pipeline = [
-        {"$group":
-            {
-                "_id": "$ip",
-                "count": {"$sum": 1}
-            }
-         },
-        {"$sort": {"count": -1}},
-        {"$limit": 10},
-        {"$project": {
-            "_id": 0,
-            "ip": "$_id",
-            "count": 1
-        }}
-    ]
-    ip_collection = nginx_collection.aggregate(pipeline)
+    # Print the number of logs for each method
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+    
+    # Print the number of GET requests to /status
+    print(f"{nginx_collection.count_documents({'method': 'GET', path: status})} status check")
+
+    # Print the top 10 most frequent IPs
     print("IPs:")
-    for ip in ip_collection:
-        print("\t{}: {}".format(ip.get("ip"), ip.get("count")))
+    top_ips = nginx_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
