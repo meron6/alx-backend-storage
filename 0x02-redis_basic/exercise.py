cf6520a91c709dc79973920ff5beac9976dd9200
meron6 <khozameron5@gmail.com>
2024-08-14 01:49:19 -0700

    update

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    def __init__(self):
+        """Initialize Redis client and flush the database."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store data in Redis with a randomly generated key."""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
