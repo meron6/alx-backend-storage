11b1db058708e639751b10199c13d25b06463bc4
meron6 <khozameron5@gmail.com>
2024-08-15 11:30:30 -0700

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,33 +1,21 @@
 #!/usr/bin/env python3
+"""
+create a web cach
+"""
 import redis
 import requests
-from typing import Callable
-from functools import wraps
+rc = redis.Redis()
+count = 0
 
-r = redis.Redis()
 
-def cache_with_count(fn: Callable) -> Callable:
-    """Decorator to cache the result of a function and count accesses."""
-
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        # Track the access count for the URL
-        r.incr(f"count:{url}")
-
-        # Check if the URL content is already cached
-        cached_page = r.get(f"cached:{url}")
-        if cached_page:
-            return cached_page.decode("utf-8")
-
-        # If not cached, get the page content and cache it
-        content = fn(url)
-        r.setex(f"cached:{url}", 10, content)
-        return content
-
-    return wrapper
-
-@cache_with_count
 def get_page(url: str) -> str:
-    """Fetch the HTML content of a URL."""
-    response = requests.get(url)
-    return response.text
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
